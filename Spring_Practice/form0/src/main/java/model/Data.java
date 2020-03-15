5b63bf7952acc52b984d096037dce42a93e4d7e9
audtjr9514 <audtjr9514@gmail.com>
2020-03-16 02:18:44 +0900

    20.03.16

---
+++ Spring_Practice/form0/src/main/java/model/Data.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Data {
+	String title;
+	int color;
+
+	public String getTitle() {
+		return this.title;
+	}
+	public int getColor() {
+		return this.color;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+}
